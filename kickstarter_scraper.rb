d87f9ede6a0c2a614442f81396c1ffcee6207099
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-16 18:52:34 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
-
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 require 'nokogiri'
 require 'pry'
